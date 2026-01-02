e6d90517fbdd2bef39f538a860aceee6641e0458
Shraddha <ranashraddha47@gmail.com>
2026-01-02 12:03:30 +0530

    Update user.js

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,1 +1,17 @@
 
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  name: String,
+  email: String,
+  mobile: String,
+  password: String, // plaintext ONLY for demo (mention in viva)
+  otp: String,
+  otpExpires: Date,
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model("User", userSchema);
